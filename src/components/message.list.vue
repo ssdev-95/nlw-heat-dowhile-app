8bf414a5919b673dde29c92945d345a2f7c4defa
Salomao S <souza95salomao@gmail.com>
2022-02-21 22:52:44 -0400

    [FEAT] Message list component

---
+++ src/components/message.list.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="message-list">
+	<!-- Begin card -->
+  <div
+    v-for="message in messages"
+    :key="message.id"
+		class="message-card"
+	>
+		<p>{{message.text}}</p>
+		<div @click="toggleBadge">
+			<img
+				:src="message.user.avatar_url"
+				alt="'user-'+message.user.github_id"
+			/>
+      <p>{{message.user.login}}</p>
+		</div>
+	</div>
+	<!-- End card -->
+</div>
+</template>
+
+<script setup lang="ts">
+import { useStore } from 'vuex'
+import { key } from '@/types'
+
+const store = useStore(key)
+const messages = store.getters.messages
+</script>
